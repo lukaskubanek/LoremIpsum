eaf90a7f171c5736968257bff296e2fdbc5122d9
Lukas Kubanek <lukas.kubanek@me.com>
2013-05-24 23:18:44 +0200

    Added a second example project for iOS

---
+++ Examples/Lorem Ipsum iOS Example/Lorem Ipsum iOS Example/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Lorem Ipsum iOS Example
+//
+//  Created by Lukas Kubanek on 24.05.2013.
+//  Copyright (c) 2013 Lukas Kubanek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ Examples/Lorem Ipsum iOS Example/Lorem Ipsum iOS Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lorem Ipsum iOS Example
+//
+//  Created by Lukas Kubanek on 24.05.2013.
+//  Copyright (c) 2013 Lukas Kubanek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
