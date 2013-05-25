9dc59b5deac49c275b646fe488835a380d85d32b
Lukas Kubanek <lukas.kubanek@me.com>
2013-05-25 12:11:43 +0200

    Added a NSTableView for displaying the text values

--- Examples/Lorem Ipsum Mac OS X Example/AppDelegate.m
+++ Examples/Lorem Ipsum Mac OS X Example/AppDelegate.m
@@ -25,30 +25,6 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     [self displayViewController:self.imagesViewController];
-    
-//    NSLog(@"1 word: %@", [LoremIpsum word]);
-//    NSLog(@"2 words: %@", [LoremIpsum wordsWithNumber:2]);
-//    NSLog(@"5 words: %@", [LoremIpsum wordsWithNumber:5]);
-//    
-//    NSLog(@"1 sentence: %@", [LoremIpsum sentence]);
-//    NSLog(@"2 sentences: %@", [LoremIpsum sentencesWithNumber:2]);
-//    NSLog(@"5 sentences: %@", [LoremIpsum sentencesWithNumber:5]);
-//    
-//    NSLog(@"1 paragraph: %@", [LoremIpsum paragraph]);
-//    NSLog(@"3 paragraphs: %@", [LoremIpsum paragraphsWithNumber:3]);
-//    
-//    NSLog(@"Title: %@", [LoremIpsum title]);
-//    
-//    NSLog(@"Date: %@", [LoremIpsum date]);
-//    
-//    NSLog(@"First name: %@", [LoremIpsum firstName]);
-//    NSLog(@"Last name: %@", [LoremIpsum lastName]);
-//    NSLog(@"Name: %@", [LoremIpsum name]);
-//    
-//    NSLog(@"Email: %@", [LoremIpsum email]);
-//    NSLog(@"URL: %@", [LoremIpsum URL]);
-//    
-//    NSLog(@"Tweet: %@", [LoremIpsum tweet]);
 }
 
 - (IBAction)changeViewController:(id)sender
--- Examples/Lorem Ipsum Mac OS X Example/TextsViewController.m
+++ Examples/Lorem Ipsum Mac OS X Example/TextsViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "TextsViewController.h"
+#import "LoremIpsum.h"
 
-@interface TextsViewController ()
+@interface TextsViewController () <NSTableViewDataSource>
 
-@property (weak) IBOutlet NSTextField *textField;
+@property (weak) IBOutlet NSTableView *tableView;
+@property (strong) NSArray *data;
 
 @end
 
@@ -19,11 +21,43 @@
 - (void)awakeFromNib
 {
     [self.view setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.tableView.dataSource = self;   
 }
 
 - (IBAction)loadText:(id)sender
 {
+    NSMutableArray *data = [NSMutableArray arrayWithCapacity:13];
+    [data addObject:@{@"type" : @"1 word", @"value" : [LoremIpsum word]}];
+    [data addObject:@{@"type" : @"5 words", @"value" : [LoremIpsum wordsWithNumber:5]}];
+    [data addObject:@{@"type" : @"1 sentence", @"value" : [LoremIpsum sentence]}];
+    [data addObject:@{@"type" : @"2 sentences", @"value" : [LoremIpsum sentencesWithNumber:2]}];
+    [data addObject:@{@"type" : @"2 paragraphs", @"value" : [LoremIpsum paragraphsWithNumber:2]}];
+    [data addObject:@{@"type" : @"Title", @"value" : [LoremIpsum title]}];
+    [data addObject:@{@"type" : @"Date", @"value" : [LoremIpsum date]}];
+    [data addObject:@{@"type" : @"First name", @"value" : [LoremIpsum firstName]}];
+    [data addObject:@{@"type" : @"Last name", @"value" : [LoremIpsum lastName]}];
+    [data addObject:@{@"type" : @"Name", @"value" : [LoremIpsum name]}];
+    [data addObject:@{@"type" : @"Email address", @"value" : [LoremIpsum email]}];
+    [data addObject:@{@"type" : @"URL", @"value" : [LoremIpsum URL]}];
+    [data addObject:@{@"type" : @"Tweet", @"value" : [LoremIpsum tweet]}];
+    self.data = [data copy];
     
+    [self.tableView reloadData];
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+    return [self.data count];
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    NSDictionary *entry = [self.data objectAtIndex:row];
+    if ([[tableColumn identifier] isEqualToString:@"type"]) {
+        return [entry valueForKey:@"type"];
+    } else {
+        return [entry valueForKey:@"value"];
+    }
 }
 
 @end
